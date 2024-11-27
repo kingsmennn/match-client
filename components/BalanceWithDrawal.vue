2fdb669e13a9fc671ac6c005d4f121b3b0c3b02b
Favour <gismozfexie@yahoo.com>
2024-11-27 02:40:21 +0100

    chore: update

--- components/BalanceWithDrawal.vue
+++ components/BalanceWithDrawal.vue
@@ -5,29 +5,33 @@ import { CoinPayment } from "@/types";
 
 const userStore = useUserStore();
 
-const balances = ref<{
+type Balance = {
   coin: CoinPayment;
   balance: number;
-}[]>([])
+}
+const balances = ref<Balance[]>([])
 const fetching = ref(true);
 const fetchBalance = async () => {
-  fetching.value = true
+  fetching.value = true;
+  balances.value = [];
   try {
-    Object.values(CoinPayment).map(async (coin) => {
+    const promises = Object.values(CoinPayment).map(async (coin) => {
       const res = await userStore.getSellerBalance(userStore.accountId!, coin);
-      console.log({coin, res})
-      balances.value.push({
-        coin,
-        balance: res
-      })
-    })
+      return { coin, balance: res };
+    });
+    const results = await Promise.all(promises);
+    balances.value = results;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   } finally {
-    fetching.value = false
+    fetching.value = false;
   }
-}
+};
 onMounted(fetchBalance)
+
+const handleWithdraw = async (coin: CoinPayment) => {
+  await userStore.withdrawSellerProfit(coin)
+}
 </script>
 
 <template>
@@ -35,12 +39,12 @@ onMounted(fetchBalance)
     class="tw-flex tw-flex-col tw-gap-3 tw-ring-1
     tw-ring-black/10 tw-rounded-xl tw-p-4 tw-bg-black/5">
     <div class="tw-flex tw-items-center tw-justify-between">
-      <h2 class="tw-text-3xl tw-font-bold">Balance</h2>
-      <!-- refresh balance -->
+      <h2 class="tw-text-3xl tw-font-bold tw-text-black/30">Balance</h2>
       <button
         @click="fetchBalance"
         class="tw-text-black tw-text-sm"
-        :class="{ 'tw-animate-spin': fetching }">
+        :class="{ 'tw-animate-spin': fetching }"
+        :disabled="fetching">
         <v-icon>mdi-refresh</v-icon>
       </button>
     </div>
@@ -48,10 +52,10 @@ onMounted(fetchBalance)
       <li
         v-for="(balance, index) in balances" :key="index"
         class="tw-ring-1 tw-ring-black/10 tw-rounded-md tw-p-4 tw-bg-white
-        tw-flex tw-items-center tw-justify-between">
-        <span>{{ balance.balance + balance.coin }}</span>
+        tw-flex tw-items-start md:tw-items-center tw-justify-between">
+        <span class="tw-uppercase">{{ balance.balance + ' ' + balance.coin }}</span>
         <button
-          @click="userStore.withdrawSellerProfit(balance.coin)"
+          @click="handleWithdraw(balance.coin)"
           class="tw-bg-black tw-text-white tw-rounded-full tw-py-2
           tw-px-4 tw-text-sm tw-font-bold"
           :class="{ 'tw-bg-black/80': fetching || balance.balance <= 0 }"
@@ -59,6 +63,14 @@ onMounted(fetchBalance)
           Withdraw
         </button>
       </li>
+      <template v-if="fetching">
+        <li
+          v-for="(item, index) in 2" :key="index"
+          class="tw-ring-1 tw-ring-black/10 tw-rounded-md tw-p-4 tw-bg-white
+          tw-uppercase tw-animate-pulse tw-font-bold tw-text-black/50">
+          <span>...</span>
+        </li>
+      </template>
     </ul>
   </div>
 </template>
