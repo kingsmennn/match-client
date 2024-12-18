0c9a853d5f5fe3e839b24861c16c100129a07796
imdavyking <yungdynamic53@gmail.com>
2024-12-18 18:40:23 +0100

    updating did

--- pinia/did.ts
+++ pinia/did.ts
@@ -7,61 +7,41 @@ import {
 import { AccountId, Hbar, PrivateKey } from "@hashgraph/sdk";
 import { useUserStore } from "./user";
 
-const getIdNetwork = async (): Promise<HcsIdentityNetwork> => {
-  const userStore = useUserStore();
-  const env = useRuntimeConfig().public;
-  const privateKey = PrivateKey.generate();
+// const getIdNetwork = async (): Promise<HcsIdentityNetwork> => {
+//   return HcsIdentityNetwork.fromAddressBook();
+// };
 
-  // Derive the public key
-  const publicKey = privateKey.publicKey;
-  const identityNetwork = await new HcsIdentityNetworkBuilder()
-    .setNetwork("testnet") // or "mainnet"
-    .setAppnetName("MatchAppnet")
-    .setPublicKey(publicKey)
-    .setMaxTransactionFee(new Hbar(2))
-    .setDidTopicMemo("MatchAppnet DID topic")
-    .setVCTopicMemo("MatchAppnet VC topic")
-    .execute(client);
+// const saveUserDetails = async () => {
+//   const identityNetwork = await getIdNetwork();
+//   const didRootKey = HcsDid.generateDidRootKey();
+//   const hcsDid = identityNetwork.generateDid(didRootKey.publicKey, true);
 
-  const receipt = await userStore.contract.hashconnect.sendTransaction(
-    AccountId.fromString(userStore.accountId!),
-    identityNetwork
-  );
+//   const phoneNumber = "+1234567890"; // Example phone number
 
-  return identityNetwork;
-};
+//   const didDocument = hcsDid.generateDidDocument();
+//   didDocument.addCustomProperty("phoneNumber", phoneNumber);
 
-const saveUserDetails = async () => {
-  const identityNetwork = await getIdNetwork();
-  const didRootKey = HcsDid.generateDidRootKey();
-  const hcsDid = identityNetwork.generateDid(didRootKey.publicKey, true);
+//   const didDocumentJson = didDocument.toJson();
+//   console.log("DID Document:", didDocumentJson);
+//   await identityNetwork
+//     .createDidTransaction(DidMethodOperation.CREATE)
+//     .setDidDocument(didDocumentJson)
+//     .signMessage((doc) => didRootKey.sign(doc))
+//     .buildAndSignTransaction((tx) => tx.setMaxTransactionFee(new Hbar(2)))
+//     .onMessageConfirmed((msg) => {
+//       console.log("DID Document published!", msg);
+//     })
+//     .execute(client);
+// };
 
-  const phoneNumber = "+1234567890"; // Example phone number
-
-  const didDocument = hcsDid.generateDidDocument();
-  didDocument.addCustomProperty("phoneNumber", phoneNumber);
-
-  const didDocumentJson = didDocument.toJson();
-  console.log("DID Document:", didDocumentJson);
-  await identityNetwork
-    .createDidTransaction(DidMethodOperation.CREATE)
-    .setDidDocument(didDocumentJson)
-    .signMessage((doc) => didRootKey.sign(doc))
-    .buildAndSignTransaction((tx) => tx.setMaxTransactionFee(new Hbar(2)))
-    .onMessageConfirmed((msg) => {
-      console.log("DID Document published!", msg);
-    })
-    .execute(client);
-};
-
-const resolveDid = async (didString: string) => {
-  const identityNetwork = await getIdNetwork();
-  const NO_MORE_MESSAGES_TIMEOUT = 15 * 1000;
-  identityNetwork
-    .getDidResolver()
-    .addDid(didString)
-    .setTimeout(NO_MORE_MESSAGES_TIMEOUT)
-    .onError(onError)
-    .whenFinished((m) => mapRef.push(m))
-    .execute(this.client);
-};
+// const resolveDid = async (didString: string) => {
+//   const identityNetwork = await getIdNetwork();
+//   const NO_MORE_MESSAGES_TIMEOUT = 15 * 1000;
+//   identityNetwork
+//     .getDidResolver()
+//     .addDid(didString)
+//     .setTimeout(NO_MORE_MESSAGES_TIMEOUT)
+//     .onError(onError)
+//     .whenFinished((m) => mapRef.push(m))
+//     .execute(this.client);
+// };
